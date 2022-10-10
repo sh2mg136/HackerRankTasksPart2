1835bdb6d07bc9a6d1aea6a69a430ad012212a1e
maxx <sh2mg@136@gmail.com>
2022-10-10 17:45:13 +0300

    Sparse Arrays

--- HackerRankTasksPart2/Program.cs
+++ HackerRankTasksPart2/Program.cs
@@ -39,10 +39,260 @@ Debug.Assert(merg.Count() == correct_list.Count());
 var dist = result_list.Except(correct_list);
 Debug.Assert(dist.Count() == 0);
 
-
 result_list = LeftRotationClass.rotateLeft(4, new List<int>() { 1, 2, 3, 4, 5 });
 correct_list = new List<int>() { 5, 1, 2, 3, 4 };
 merg = result_list.Intersect(correct_list);
 Debug.Assert(merg.Count() == correct_list.Count());
 dist = result_list.Except(correct_list);
 Debug.Assert(dist.Count() == 0);
+
+//
+//
+// Sparse Arrays
+// https://www.hackerrank.com/challenges/sparse-arrays/problem?isFullScreen=true
+List<int> ints;
+List<int> corr;
+ints = SparseArraysClass.matchingStrings(new List<string>() { "ab", "ab", "abc" }, new List<string>() { "ab", "abc", "bc" });
+corr = new List<int> { 2, 1, 0 };
+merg = ints.Intersect(corr);
+Debug.Assert(merg.Count() == corr.Count());
+dist = ints.Except(corr);
+Debug.Assert(dist.Count() == 0);
+
+ints = SparseArraysClass.matchingStrings(
+    new List<string>() { "aba", "baba", "aba", "xzxb" },
+    new List<string>() { "aba", "xzxb", "ab" });
+corr = new List<int> { 2, 1, 0 };
+merg = ints.Intersect(corr);
+Debug.Assert(merg.Count() == corr.Count());
+dist = ints.Except(corr);
+Debug.Assert(dist.Count() == 0);
+
+ints = SparseArraysClass.matchingStrings(
+    new List<string>() { "def", "de", "fgh" },
+    new List<string>() { "de", "lmn", "fgh" });
+corr = new List<int> { 1, 0, 1 };
+merg = ints.Intersect(corr).ToList();
+Debug.Assert(merg.Count() == corr.Distinct().Count());
+dist = ints.Except(corr);
+Debug.Assert(dist.Count() == 0);
+
+ints = SparseArraysClass.matchingStrings(new List<string>()
+{
+"lekgdisnsbfdzpqlkg",
+"eagemhdygyv",
+"jwvwwnrhuai",
+"urcadmrwlqe",
+"mpgqsvxrijpombyv",
+"mpgqsvxrijpombyv",
+"urcadmrwlqe",
+"mpgqsvxrijpombyv",
+"eagemhdygyv",
+"eagemhdygyv",
+"jwvwwnrhuai",
+"urcadmrwlqe",
+"jwvwwnrhuai",
+"kvugevicpsdf",
+"kvugevicpsdf",
+"mpgqsvxrijpombyv",
+"urcadmrwlqe",
+"mpgqsvxrijpombyv",
+"exdafbnobg",
+"qhootohpnfvbl",
+"suffrbmqgnln",
+"exdafbnobg",
+"exdafbnobg",
+"eagemhdygyv",
+"mpgqsvxrijpombyv",
+"urcadmrwlqe",
+"jwvwwnrhuai",
+"lekgdisnsbfdzpqlkg",
+"mpgqsvxrijpombyv",
+"lekgdisnsbfdzpqlkg",
+"jwvwwnrhuai",
+"exdafbnobg",
+"mpgqsvxrijpombyv",
+"kvugevicpsdf",
+"qhootohpnfvbl",
+"urcadmrwlqe",
+"kvugevicpsdf",
+"mpgqsvxrijpombyv",
+"lekgdisnsbfdzpqlkg",
+"mpgqsvxrijpombyv",
+"kvugevicpsdf",
+"qhootohpnfvbl",
+"lxyqetmgdbmh",
+"urcadmrwlqe",
+"urcadmrwlqe",
+"kvugevicpsdf",
+"lxyqetmgdbmh",
+"urcadmrwlqe",
+"lxyqetmgdbmh",
+"jwvwwnrhuai",
+"qhootohpnfvbl",
+"qhootohpnfvbl",
+"jwvwwnrhuai",
+"lekgdisnsbfdzpqlkg",
+"kvugevicpsdf",
+"mpgqsvxrijpombyv",
+"exdafbnobg",
+"kvugevicpsdf",
+"lekgdisnsbfdzpqlkg",
+"qhootohpnfvbl",
+"exdafbnobg",
+"qhootohpnfvbl",
+"exdafbnobg",
+"mpgqsvxrijpombyv",
+"suffrbmqgnln",
+"mpgqsvxrijpombyv",
+"qhootohpnfvbl",
+"jwvwwnrhuai",
+"mpgqsvxrijpombyv",
+"qhootohpnfvbl",
+"lekgdisnsbfdzpqlkg",
+"eagemhdygyv",
+"jwvwwnrhuai",
+"kvugevicpsdf",
+"eagemhdygyv",
+"eagemhdygyv",
+"lxyqetmgdbmh",
+"qhootohpnfvbl",
+"lxyqetmgdbmh",
+"exdafbnobg",
+"qhootohpnfvbl",
+"qhootohpnfvbl",
+"exdafbnobg",
+"suffrbmqgnln",
+"mpgqsvxrijpombyv",
+"urcadmrwlqe",
+"eagemhdygyv",
+"lxyqetmgdbmh",
+"urcadmrwlqe",
+"suffrbmqgnln",
+"qhootohpnfvbl",
+"kvugevicpsdf",
+"lekgdisnsbfdzpqlkg",
+"lxyqetmgdbmh",
+"mpgqsvxrijpombyv",
+"jwvwwnrhuai",
+"lxyqetmgdbmh",
+"lxyqetmgdbmh",
+"lekgdisnsbfdzpqlkg",
+"qhootohpnfvbl"
+},
+new List<string>()
+{
+"exdafbnobg",
+"eagemhdygyv",
+"mpgqsvxrijpombyv",
+"kvugevicpsdf",
+"lekgdisnsbfdzpqlkg",
+"kvugevicpsdf",
+"exdafbnobg",
+"qhootohpnfvbl",
+"eagemhdygyv",
+"kvugevicpsdf",
+"suffrbmqgnln",
+"jwvwwnrhuai",
+"lekgdisnsbfdzpqlkg",
+"lekgdisnsbfdzpqlkg",
+"mpgqsvxrijpombyv",
+"jwvwwnrhuai",
+"kvugevicpsdf",
+"lekgdisnsbfdzpqlkg",
+"exdafbnobg",
+"suffrbmqgnln",
+"qhootohpnfvbl",
+"eagemhdygyv",
+"exdafbnobg",
+"suffrbmqgnln",
+"jwvwwnrhuai",
+"qhootohpnfvbl",
+"eagemhdygyv",
+"exdafbnobg",
+"exdafbnobg",
+"jwvwwnrhuai",
+"qhootohpnfvbl",
+"lxyqetmgdbmh",
+"qhootohpnfvbl",
+"suffrbmqgnln",
+"lxyqetmgdbmh",
+"qhootohpnfvbl",
+"eagemhdygyv",
+"jwvwwnrhuai",
+"eagemhdygyv",
+"qhootohpnfvbl",
+"mpgqsvxrijpombyv",
+"qhootohpnfvbl",
+"jwvwwnrhuai",
+"exdafbnobg",
+"eagemhdygyv",
+"eagemhdygyv",
+"kvugevicpsdf",
+"kvugevicpsdf",
+"jwvwwnrhuai",
+"urcadmrwlqe",
+"lxyqetmgdbmh",
+"qhootohpnfvbl",
+"exdafbnobg",
+"exdafbnobg",
+"eagemhdygyv",
+"qhootohpnfvbl",
+"exdafbnobg",
+"exdafbnobg",
+"lekgdisnsbfdzpqlkg",
+"jwvwwnrhuai",
+"eagemhdygyv",
+"urcadmrwlqe",
+"kvugevicpsdf",
+"lekgdisnsbfdzpqlkg",
+"jwvwwnrhuai",
+"eagemhdygyv",
+"lekgdisnsbfdzpqlkg",
+"exdafbnobg",
+"kvugevicpsdf",
+"jwvwwnrhuai",
+"exdafbnobg",
+"lxyqetmgdbmh",
+"exdafbnobg",
+"lxyqetmgdbmh",
+"jwvwwnrhuai",
+"mpgqsvxrijpombyv",
+"eagemhdygyv",
+"urcadmrwlqe",
+"kvugevicpsdf",
+"qhootohpnfvbl",
+"jwvwwnrhuai",
+"eagemhdygyv",
+"urcadmrwlqe",
+"urcadmrwlqe",
+"exdafbnobg",
+"qhootohpnfvbl",
+"exdafbnobg",
+"eagemhdygyv",
+"exdafbnobg",
+"jwvwwnrhuai",
+"eagemhdygyv",
+"jwvwwnrhuai",
+"mpgqsvxrijpombyv",
+"urcadmrwlqe",
+"urcadmrwlqe",
+"eagemhdygyv",
+"eagemhdygyv",
+"jwvwwnrhuai",
+"suffrbmqgnln",
+"eagemhdygyv"
+});
+
+corr = new List<int>() { 9, 8, 16, 10, 9, 10, 9, 14, 8, 10, 4, 10, 9, 9, 16, 10, 10, 9, 9,
+4, 14, 8, 9, 4, 10, 14, 8, 9, 9, 10, 14, 9, 14, 4, 9, 14, 8, 10, 8, 14, 16, 14, 10, 9, 8,
+8, 10, 10, 10, 11, 9, 14, 9, 9, 8, 14, 9, 9, 9, 10, 8, 11, 10, 9, 10, 8, 9, 9, 10, 10, 9,
+9, 9, 9, 10, 16, 8, 11, 10, 14, 10, 8, 11, 11, 9, 14, 9, 8, 9, 10, 8, 10, 16, 11, 11, 8,
+8, 10, 4, 8 };
+
+merg = ints.Intersect(corr).ToList();
+Debug.Assert(merg.Count() == corr.Distinct().Count());
+dist = ints.Except(corr);
+Debug.Assert(dist.Count() == 0);
+
+
---
+++ HackerRankTasksPart2/SparseArraysClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankTasksPart2
+{
+    internal class SparseArraysClass
+    {
+        public static List<int> matchingStrings(List<string> stringList, List<string> queries)
+        {
+            var result = new List<int>();
+            int c = 0;
+            foreach (var str in queries)
+            {
+                c = stringList.Where(x => x == str).Count();
+                result.Add(c);
+            }
+            return result;
+        }
+    }
+}
