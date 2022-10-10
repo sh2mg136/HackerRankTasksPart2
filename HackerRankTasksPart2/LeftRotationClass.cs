860550f73819f4c676edca9d7af16703973d5ec6
maxx <sh2mg@136@gmail.com>
2022-10-10 17:09:26 +0300

    Left Rotation

---
+++ HackerRankTasksPart2/LeftRotationClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankTasksPart2
+{
+    internal class LeftRotationClass
+    {
+        public static List<int> rotateLeft(int d, List<int> arr)
+        {
+            var result = new List<int>();
+            var shift = d % arr.Count;
+            var ind = 0;
+            for (int i = shift; i < arr.Count + shift; i++)
+            {
+                ind = i;
+                if (ind >= arr.Count)
+                    ind %= arr.Count;
+                result.Add(arr[ind]);
+            }
+            return result;
+        }
+    }
+}
--- HackerRankTasksPart2/Program.cs
+++ HackerRankTasksPart2/Program.cs
@@ -26,3 +26,23 @@ Debug.Assert(mer.Count() == 4);
 var dis = result.Except(correct);
 Debug.Assert(dis.Count() == 0);
 
+//
+//
+// Left Rotation
+// https://www.hackerrank.com/challenges/array-left-rotation/problem?isFullScreen=true
+List<int> result_list;
+List<int> correct_list;
+result_list = LeftRotationClass.rotateLeft(2, new List<int>() { 1, 2, 3, 4, 5 });
+correct_list = new List<int>() { 3, 4, 5, 1, 2 };
+var merg = result_list.Intersect(correct_list);
+Debug.Assert(merg.Count() == correct_list.Count());
+var dist = result_list.Except(correct_list);
+Debug.Assert(dist.Count() == 0);
+
+
+result_list = LeftRotationClass.rotateLeft(4, new List<int>() { 1, 2, 3, 4, 5 });
+correct_list = new List<int>() { 5, 1, 2, 3, 4 };
+merg = result_list.Intersect(correct_list);
+Debug.Assert(merg.Count() == correct_list.Count());
+dist = result_list.Except(correct_list);
+Debug.Assert(dist.Count() == 0);
