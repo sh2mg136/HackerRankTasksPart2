9b02bbdef1bd3881dcd4b7157781c6cade572c4a
maxx <sh2mg@136@gmail.com>
2022-10-11 18:40:18 +0300

    Jumping on the Clouds

---
+++ JumpingOnTheClouds/JumpingOnTheCloudsClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingOnTheClouds
+{
+    internal class JumpingOnTheCloudsClass
+    {
+        public static int jumpingOnClouds(List<int> c)
+        {
+            int result = 0;
+            int cnt = 0;
+            while (cnt < c.Count - 1)
+            {
+                if (cnt + 2 < c.Count && c[cnt + 2] == 0)
+                    cnt ++;
+                result++;
+                cnt++;
+            }
+            return result;
+        }
+    }
+}
---
+++ JumpingOnTheClouds/Program.cs
@@ -0,0 +1,15 @@
+﻿using JumpingOnTheClouds;
+using System.Diagnostics;
+
+//
+//
+// Jumping on the Clouds
+// https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?isFullScreen=true
+Console.WriteLine("Jumping on the Clouds");
+int result = 0;
+
+result = JumpingOnTheCloudsClass.jumpingOnClouds(new List<int>() { 0, 1, 0, 0, 0, 1, 0 });
+Debug.Assert(result == 3);
+
+result = JumpingOnTheCloudsClass.jumpingOnClouds(new List<int>() { 0, 0, 1, 0, 0, 1, 0 });
+Debug.Assert(result == 4);
