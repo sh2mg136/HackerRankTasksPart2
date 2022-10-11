7d26d8f8def2425d14bb4192c05ec2e8f6cad4c6
maxx <sh2mg@136@gmail.com>
2022-10-11 16:35:14 +0300

    insertNodeAtHead + insertNodeAtTail

---
+++ LinkedListTasks/LinkedListSolution.cs
@@ -0,0 +1,68 @@
+﻿namespace LinkedListTasks
+{
+    internal class LinkedListSolution
+    {
+        internal static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+        {
+            var tmp = head;
+            if (head == null)
+                return new SinglyLinkedListNode(data);
+
+            while (tmp.next != null)
+                tmp = tmp.next;
+            tmp.next = new SinglyLinkedListNode(data);
+            return head;
+        }
+
+
+        internal static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data)
+        {
+            if (llist == null)
+                return new SinglyLinkedListNode(data);
+
+            var cur = llist;
+            llist = new SinglyLinkedListNode(data);
+            llist.next = cur;
+
+            return llist;
+        }
+
+
+        internal static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+        {
+            while (node != null)
+            {
+                textWriter.Write(node.data);
+                node = node.next;
+                if (node != null)
+                {
+                    textWriter.Write(sep);
+                }
+            }
+        }
+    }
+
+    internal class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    internal class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+    }
+}
---
+++ LinkedListTasks/Program.cs
@@ -0,0 +1,24 @@
+﻿using LinkedListTasks;
+using System.Diagnostics;
+
+//
+//
+// Insert a Node at the Tail of a Linked List
+// https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=true
+Console.WriteLine("Insert a Node at the Tail of a Linked List");
+
+SinglyLinkedList llist = new SinglyLinkedList();
+
+for (int i = 0; i < 3; i++)
+{
+    int llistItem = i * 2 + 1;
+
+    SinglyLinkedListNode llist_head = LinkedListSolution.insertNodeAtHead(llist.head, llistItem);
+    // SinglyLinkedListNode llist_head = LinkedListSolution.insertNodeAtTail(llist.head, llistItem);
+
+    llist.head = llist_head;
+}
+
+LinkedListSolution.PrintSinglyLinkedList(llist.head, "\n", Console.Out);
+
+
