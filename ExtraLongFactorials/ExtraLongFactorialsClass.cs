415ad19c1dee9a9ef79a4ffbcf0d7b1ac64c975a
maxx <sh2mg@136@gmail.com>
2022-10-11 17:24:10 +0300

    Extra Long Factorials (BigInteger)

---
+++ ExtraLongFactorials/ExtraLongFactorialsClass.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    internal class ExtraLongFactorialsClass
+    {
+        public static string Run(int n)
+        {
+            if (n <= 20)
+            {
+                var res = fact(n);
+                return res.ToString();
+            }
+            else
+            {
+                BigInteger b = longfact(n);
+                return b.ToString();
+            }
+        }
+
+        private static long fact(int i)
+        {
+            if (i <= 1) return 1;
+            long result = 0;
+            result += i * fact(i - 1);
+            return result;
+        }
+
+        private static BigInteger longfact(int n)
+        {
+            if (n <= 1) return 1;
+            BigInteger b = BigInteger.One;
+            for (int i = 2; i <= n; i++)
+            {
+                b = BigInteger.Multiply(new BigInteger(i), b);
+            }
+            return b;
+        }
+    }
+}
---
+++ ExtraLongFactorials/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExtraLongFactorials;
+using System.Diagnostics;
+
+
+//
+//
+// Extra Long Factorials
+// https://www.hackerrank.com/challenges/extra-long-factorials/problem?isFullScreen=true
+Console.WriteLine("Extra Long Factorials");
+string str = ExtraLongFactorialsClass.Run(4);
+Debug.Assert(str == "24");
+
+str = ExtraLongFactorialsClass.Run(9);
+Debug.Assert(str == "362880");
+
+str = ExtraLongFactorialsClass.Run(10);
+Debug.Assert(str == "3628800");
+
+str = ExtraLongFactorialsClass.Run(20);
+Debug.Assert(str == "2432902008176640000");
+
+str = ExtraLongFactorialsClass.Run(21);
+Debug.Assert(str == "51090942171709440000");
+
+str = ExtraLongFactorialsClass.Run(25);
+Debug.Assert(str == "15511210043330985984000000");
