083f4574b6a3dbaa9fdfbecb11656d02f9fc484a
maxx <sh2mg@136@gmail.com>
2022-10-15 13:46:13 +0300

    Modified Kaprekar Numbers

---
+++ EqualizeTheArray/ModifiedKaprekarNumbers.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualizeTheArray
+{
+    internal class ModifiedKaprekarNumbersClass
+    {
+        public static string kaprekarNumbers(int p, int q)
+        {
+            List<long> k = new List<long>();
+
+            for (int i = p; i <= q; i++)
+            {
+                var kaprekarNumber = Find(i);
+                if (kaprekarNumber > 0)
+                {
+                    k.Add(kaprekarNumber);
+                }
+            }
+
+            var result = String.Join(" ", k);
+            if (k.Count == 0)
+                result = "INVALID RANGE";
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static bool checkKaprekarNumbers(int p, int q)
+        {
+            bool hasKaprekar = false;
+            List<long> k = new List<long>();
+
+            for (int i = p; i <= q; i++)
+            {
+                var kaprekarNumber = Find(p);
+                if (kaprekarNumber > 0)
+                {
+                    hasKaprekar = true;
+                    k.Add(kaprekarNumber);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", k));
+
+            return hasKaprekar;
+        }
+
+        static long Find(long p)
+        {
+            long num = p * p;
+            var len = num.ToString().Length;
+            List<Tuple<int, int>> pairs = new List<Tuple<int, int>>();
+            List<long> nums = new List<long>();
+
+            if (len == 1)
+            {
+                nums = new List<long>() { num };
+            }
+            else
+            {
+                if (len % 2 == 0)
+                {
+                    pairs.Add(new Tuple<int, int>(len / 2, len / 2));
+                }
+                else
+                {
+                    int l = len / 2;
+                    pairs.Add(new Tuple<int, int>(l, l + 1));
+                    pairs.Add(new Tuple<int, int>(l + 1, l));
+                }
+
+                var s = num.ToString();
+                StringBuilder sb = new StringBuilder();
+                long a, b;
+                foreach (var t in pairs)
+                {
+                    for (int i = 0; i < t.Item1; i++)
+                        sb.Append(s[i]);
+                    a = Convert.ToInt64(sb.ToString());
+
+                    sb.Clear();
+                    for (int i = t.Item1; i < t.Item1 + t.Item2; i++)
+                        sb.Append(s[i]);
+                    var secondStr = sb.ToString().TrimStart('0');
+                    if (string.IsNullOrEmpty(secondStr))
+                        b = 0;
+                    else
+                        b = Convert.ToInt64(secondStr);
+
+                    if (a > 0 && b > 0)
+                    {
+                        nums.Add(a + b);
+                    }
+                }
+            }
+
+            bool result;
+            foreach (var pair in nums)
+            {
+                result = pair == p;
+                if (result)
+                    return p;
+            }
+            return 0;
+        }
+    }
+}
--- EqualizeTheArray/Program.cs
+++ EqualizeTheArray/Program.cs
@@ -2,7 +2,56 @@
 using System.Diagnostics;
 using System.Linq;
 
+//
+//
+// Modified Kaprekar Numbers
+// https://www.hackerrank.com/challenges/kaprekar-numbers/problem?isFullScreen=true
+Console.WriteLine("Modified Kaprekar Numbers");
+bool hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(1, 1);
+Debug.Assert(hasKaprekar);
 
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(5, 5);
+Debug.Assert(!hasKaprekar);
+
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(9, 9);
+Debug.Assert(hasKaprekar);
+
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(10, 10);
+Debug.Assert(!hasKaprekar);
+
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(44, 44);
+Debug.Assert(!hasKaprekar);
+
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(45, 45);
+Debug.Assert(hasKaprekar);
+
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(55, 55);
+Debug.Assert(hasKaprekar);
+
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(999, 999);
+Debug.Assert(hasKaprekar);
+
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(2223, 2223);
+Debug.Assert(hasKaprekar);
+
+hasKaprekar = ModifiedKaprekarNumbersClass.checkKaprekarNumbers(99999, 99999);
+Debug.Assert(hasKaprekar);
+
+
+var s = ModifiedKaprekarNumbersClass.kaprekarNumbers(1, 100);
+Debug.Assert(s == "1 9 45 55 99");
+
+s = ModifiedKaprekarNumbersClass.kaprekarNumbers(99999, 99999);
+Debug.Assert(s == "99999");
+
+s = ModifiedKaprekarNumbersClass.kaprekarNumbers(1, 99999);
+Debug.Assert(s == "1 9 45 55 99 297 703 999 2223 2728 4950 5050 7272 7777 9999 17344 22222 77778 82656 95121 99999");
+
+
+
+//
+//
+// Sort emplyees (certificate task)
 var eList = EmployeeOperations.OldestAgeForEachCompany2(EmployeeOperations.Employees);
 Debug.Assert(eList.Count > 0);
 foreach (var e in eList)
